bacacd5978ad274d96044f174d9eb361110fea1b
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2026-02-06 09:47:08 +0200

    Add movement to the game

--- game/game.js
+++ game/game.js
@@ -1,5 +1,35 @@
 const canvas = document.getElementById("game-canvas");
 const context = canvas.getContext("2d");
 
-context.fillStyle = "#2A2C24";
-context.fillRect(0, 0, 800, 600);
+const playerWidth  = 70;
+const playerHeight = 100;
+
+let playerX = 400;
+let playerY = 300;
+
+addEventListener("keydown", (event) => {
+    if (event.key === "a")
+        playerX -= 5;
+    else if (event.key === "d")
+        playerX += 5;
+});
+
+function update()
+{
+    if ((playerX + playerWidth) >= 800)
+    {
+        playerX = 800 - playerWidth;
+    }
+
+    context.clearRect(0, 0, 800, 600);
+    context.fillStyle = "#2A2C24";
+    context.fillRect(
+        playerX - (playerWidth  / 2),
+        playerY - (playerHeight / 2),
+        playerWidth,
+        playerHeight);
+
+    requestAnimationFrame(update);
+}
+
+update();
